5a65cb59eaa64af006ec3a6750292d9394cbeaf2
Neel Kumar <neelkumar247me@gmail.com>
2026-08-13 10:07:05 +0530

    Add logic to format pull request comment

---
+++ scripts/comment_formatter.py
@@ -0,0 +1,18 @@
+def format_pr_comment(
+          analyses: list[dict]
+) -> str:
+     """
+     Format analyses into a PR comment
+     """
+     comment = "## CI Failure Analysis 👀\n\n"
+
+     comment += "> Automatically analysed by the CI Failure Analyser\n\n"
+
+     for analysis in analyses:
+        comment += f"### ❌ {analysis['job_name']}\n\n"
+        comment += f"Diagnosis\n{analysis["diagnosis"]}"
+        comment += "\n\n - - - - - - - - - - - - - - -\n\n"
+
+     comment += "*Analysis powered by Groq AI. Always verify before applying fixes*"
+
+     return comment
